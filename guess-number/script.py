3f14e4a65824a1ff7be01df17a098b720b34ce37
esoty6 <kamilwitek12@o2.pl>
2023-12-07 21:50:08 +0100

    guess-number

---
+++ guess-number/script.py
@@ -0,0 +1,19 @@
+import random
+
+randomNumber = random.randint(0, 100)
+
+def get_user_guess():
+  global userGuess
+  userGuess = int(input('Guess number: '))
+
+get_user_guess()
+
+while userGuess != randomNumber:
+  if userGuess < randomNumber:
+    print('Try again! You guessed too small')
+  else:
+    print('Try again! You guessed too big')
+  get_user_guess()
+  
+print("Congrats you guessed the number!")
+
